b64e58c8f9da4c604b38b9e8bb67c24eb79e0822
Abhishek Thapliyal <at7451189@gmail.com>
2016-02-17 14:45:28 +0530

    LOGIN

---
+++ src/java/JAVA_CLASSES/DBManger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+jdbc:mysql://localhost:3306/?user=root 
+*/
+
+package JAVA_CLASSES;
+import JAVA_CLASSES.LoginManager;
+/**
+ *
+ * @author ABHISHEK
+ */
+public class DBManger {
+    
+    public void login(String user, String passowrd)
+    {
+        LoginManager loginManager = new LoginManager();
+        loginManager.login(user, passowrd);
+    }
+    
+}
